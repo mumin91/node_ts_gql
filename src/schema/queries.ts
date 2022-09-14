851d7b71707f70346f8518b86e5be1e3355b1874
Mumin <r.muminur@gmail.com>
2022-09-14 14:00:50 +0600

    Add get question list query

--- src/schema/queries.ts
+++ src/schema/queries.ts
@@ -7,14 +7,30 @@ import {
   printSchema,
   GraphQLList,
 } from 'graphql';
+import ChoiceModel from '../db/models/choice-model';
+import QuestionModel from '../db/models/question-model';
 
 import Question from './types/question';
 
 const QueryType = new GraphQLObjectType({
     name: 'Query',
     fields: {
-        questions: {
-            type: new GraphQLList(new GraphQLNonNull(Question))
+        getQuestions: {
+            type: new GraphQLList(new GraphQLNonNull(Question)),
+            resolve:async (source:any) => {
+                const questions = await QuestionModel.findAll({
+                    attributes: ['id', 'text', 'publishDate'],
+                    order: ['publishDate'],
+                    include: [ChoiceModel]
+                });
+                
+                const response = questions.map(question => {
+                    return { ...question.toJSON(), choices: question.get("Choices") };
+                })
+                console.log(response);
+
+                return response;
+            }
         }
     },
 })
